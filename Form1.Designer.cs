0ab0ddb965ae3f4fdbdfdcb5a981c9ee8f50643e
Tudor Caloian <tudor.caloian@gmail.com>
2021-03-09 03:11:24 +0200

    Minesweeper

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Minesweeper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(87, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "10";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 38);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "tiles ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox3.CausesValidation = false;
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(307, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(55, 38);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "10";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.HideSelection = false;
+            this.textBox4.Location = new System.Drawing.Point(232, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 38);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "flags";
+            // 
+            // Minesweeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 461);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Minesweeper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Minesweeper : Form
+    {
+        public Minesweeper()
+        {
+            InitializeComponent();
+           // Game();
+           // Click();
+        }
+
+
+
+       public static int m = 100;
+        int n = 10, mines = 20, flags = 10;
+        Button[,] b = new Button[m, m];
+        int[,] a = new int[m, m];
+        bool[,] bomb = new bool[m ,m];
+        bool[,] flag = new bool[m, m];
+        int[] di= new int[] {0, 0, -1, 1, 1, 1, -1, -1};
+        int[] dj = new int[] {-1, 1, 0, 0, 1, -1, -1, 1};
+        bool[,] check = new bool[m, m];
+        int cnt = 0;
+         int width = 40;
+         int height = 40;
+         
+        int startw = 10;
+        int starth = 50;
+        bool start = true;    
+        struct pi
+        {
+            public int first, second;
+        }
+        
+
+        public void Easy()
+        {
+            n = 10;
+            mines = 20;
+            flags = 40;
+            cnt = n * n;
+            Game();
+        }
+
+        public void Normal()
+        {
+            n = 15;
+            mines = 35;
+            flags = 70;
+            this.Size = new Size(638, 700);
+            cnt = n * n;
+            Game();
+        }
+        public void Hard()
+        {
+            n = 20;
+            mines = 70;
+            flags = 100;
+            this.Size = new Size(840, 900);
+            cnt = n * n;
+            Game();
+        }
+
+       
+        public void Game()
+        {
+
+           // Easy();
+           // cnt = n * n;
+            textBox1.Text = cnt.ToString();
+            textBox3.Text = flags.ToString();
+            
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    Button button= new Button();
+                    button.Top = (starth+x * height );
+                    button.Left = (startw+y * width );
+                    button.Width = width;
+                    button.Height = height;
+                    button.BackColor = Color.CornflowerBlue;
+                    button.TabStop = false;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 1;
+                    button.FlatAppearance.BorderColor = Color.DarkOrange;
+                    button.MouseUp += new MouseEventHandler(Click);
+                    Controls.Add(button);
+
+                    b[x, y] = button; 
+                }
+
+            }
+
+
+        }
+
+        private void Click(object sender, MouseEventArgs e)
+        {
+            Point coord = ((Button)sender).Location;
+            int x = (coord.Y-starth)/width;
+            int y = (coord.X-startw)/height;
+            if(start == true)
+            {
+                Random r = new Random();
+                int i, j;
+
+                while (mines > 0)
+                {
+                    i = r.Next(1, n-2);
+                    j = r.Next(1, n-2);
+                    if (!bomb[i, j] && !(i == x && j == y) && !(i == x && j+1 == y) && !(i == x && j - 1 == y) && !(i + 1 == x && j == y) && !(i - 1 == x && j == y) )
+                    {
+                        bomb[i, j] = true;
+                        a[i + 1, j]++;
+                        a[i, j + 1]++;
+                        a[i - 1, j]++;
+                        a[i, j - 1]++;
+                        mines--;
+
+                    }
+
+
+                }
+                start = false;
+            }
+            if(!flag[x, y])
+            if (e.Button == MouseButtons.Left)
+            {
+                if (check[x, y] == false)
+                {
+
+
+                    if (bomb[x, y])
+                    {
+
+                        
+                        b[x, y].BackColor = Color.Black;
+                        b[x, y].Text = "BOMB";
+                        b[x, y].BackgroundImage = Properties.Resources.bomb;
+                        b[x, y].ForeColor = Color.White;
+                        b[x, y].Font = new Font("Arial", 6, FontStyle.Bold);
+                        MessageBox.Show("You lost...");
+                        this.Close();
+
+                    }
+                    else
+                    {
+                        b[x, y].BackColor = Color.Red;
+                        Queue<pi> q = new Queue<pi>();
+                        q.Enqueue(new pi { first = x, second = y });
+
+                        while (q.Count > 0)
+                        {
+                            pi tile = q.Peek();
+                            q.Dequeue();
+                            int i, j;
+                            i = tile.first;
+                            j = tile.second;
+                            if (check[i, j] == true) continue;
+                            check[i, j] = true;
+                            cnt--;
+                            textBox1.Text = cnt.ToString();
+                            if (i < 0 || j < 0 || i >= n || j >= n)
+                                continue;
+                            b[i, j].BackColor = Color.LightSlateGray;
+                            if (a[i, j] > 0)
+                            {
+                                b[i, j].Text = a[i, j].ToString();
+                                continue;
+                            }
+
+                            for (int k = 0; k < 4; k++)
+                            {
+                                int i_next, j_next;
+                                i_next = i + di[k];
+                                j_next = j + dj[k];
+                                if (i_next < 0 || j_next < 0 || i_next >= n || j_next >= n)
+                                    continue;
+                                q.Enqueue(new pi { first = i_next, second = j_next });
+
+                            }
+
+                        }
+
+                    }
+
+
+                    if (cnt == 0)
+                    {
+                        MessageBox.Show("Congratulations. You WON!!!");
+                        this.Close();
+                    }
+                }
+            }
+            else
+            {
+                if(flags > 0)
+                {
+                    flags--;
+                    cnt--;
+                    flag[x, y] = true;
+                    textBox1.Text = cnt.ToString();
+                    textBox3.Text = flags.ToString();
+                    b[x, y].BackgroundImage = Properties.Resources.flag;
+
+
+
+
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+   
+}
